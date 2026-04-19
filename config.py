3ec2747d2c26207a94f73ffd378bac0ce0dcd2fa
Gustavo Denani <gustavohdenani@gmail.com>
2026-04-19 12:14:04 -0300

    Restore pipeline with new Neon credentials

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+DB_CONFIG = {
+    "host":     os.environ.get("DB_HOST", "ep-frosty-water-am38vdoe-pooler.c-5.us-east-1.aws.neon.tech"),
+    "port":     int(os.environ.get("DB_PORT", 5432)),
+    "dbname":   os.environ.get("DB_NAME", "neondb"),
+    "user":     os.environ.get("DB_USER", "neondb_owner"),
+    "password": os.environ.get("DB_PASSWORD", "npg_vD84nMVrfmXa"),
+    "sslmode":  "require"
+}
