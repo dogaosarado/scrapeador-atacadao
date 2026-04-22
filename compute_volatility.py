88f839b9edfcd52fe08afce41e7282c46de60d4a
Gustavo Denani <gustavohdenani@gmail.com>
2026-04-21 23:41:01 -0300

    Remove hardcoded credentials

--- compute_volatility.py
+++ compute_volatility.py
@@ -8,11 +8,11 @@ MIN_SAMPLE = 10
 
 def get_conn():
     return psycopg2.connect(
-        host=os.environ.get("DB_HOST", "ep-frosty-water-am38vdoe-pooler.c-5.us-east-1.aws.neon.tech"),
+        host=os.environ["DB_HOST"],
         port=int(os.environ.get("DB_PORT", 5432)),
-        dbname=os.environ.get("DB_NAME", "neondb"),
-        user=os.environ.get("DB_USER", "neondb_owner"),
-        password=os.environ.get("DB_PASSWORD", "Atacadao2025"),
+        dbname=os.environ["DB_NAME"],
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASSWORD"],
         sslmode="require"
     )
 
--- scraper.py
+++ scraper.py
@@ -18,11 +18,11 @@ PAGE_SIZE = 50
 
 def get_conn():
     return psycopg2.connect(
-        host=os.environ.get("DB_HOST", "ep-frosty-water-am38vdoe-pooler.c-5.us-east-1.aws.neon.tech"),
+        host=os.environ["DB_HOST"],
         port=int(os.environ.get("DB_PORT", 5432)),
-        dbname=os.environ.get("DB_NAME", "neondb"),
-        user=os.environ.get("DB_USER", "neondb_owner"),
-        password=os.environ.get("DB_PASSWORD", "Atacadao2024"),
+        dbname=os.environ["DB_NAME"],
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASSWORD"],
         sslmode="require"
     )
 
